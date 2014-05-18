a9ff8813d7da72838a9f64ba3be81cd258569bfc
Walker Mellema <walkermellema@gmail.com>
2014-05-18 12:55:56 -0500

    working on array stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,56 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
+  
+  if(typeof obj === 'number'){
+    return obj.toString();
+  }
+  
+  if(obj === null){
+    return 'null';
+  }
+  
+  if(typeof obj === 'string'){
+    return "\"" + obj + "\"";
+  }
+  
+  if(Array.isArray(obj)){
+    var arrayString = "";
+    var arr = obj.slice();
+    var arrOut = recursArray(arr, arrayString);
+    return "[" + arrOut + "]";
+  }
+  
+  if(typeof obj === 'boolean'){
+    return obj.toString();
+  }
   // your code goes here
 };
+
+var recursArray = function(arr, str){
+  
+  if(arr.length){
+    var s = arr.shift();
+    if(typeof s === 'string'){
+      str = str + "\"" + s + "\"";
+    }
+    else{
+      str = str + s;
+    }
+    
+  /*console.log('meal before bite:', meal);
+  console.log('now eating', meal.pop());
+  if(meal.length){
+    eat(meal);
+  } else {
+    console.log('done with the meal!');*/
+    recursArray(arr, str);
+  }
+  
+  if(str.length > 0){
+    str = str.splice(str.length - 1, 1);
+  }
+  
+  
+  return str;
+}
