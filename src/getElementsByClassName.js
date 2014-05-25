982cad148a614392a675f0a2cca2ba393cf7e575
Walker Mellema <walkermellema@gmail.com>
2014-05-25 11:57:09 -0700

    beginning to believe

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,20 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var arr = [];
+  var $body = $(document.body);
+  //var $kids = ($body).children();
+
+  
+  $($body).each(function(index, element){
+    if(element.classList.contains(className)){
+      arr.push(element);
+    }
+    if(element.childNodes){
+      var check = "yes";
+    }
+    //arr = arr.concat(getElementsByClassName(className));
+  });
+  
+  return arr;
 };
