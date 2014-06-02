7b5ca47f874a5c6f15569e05cdb5db21d06819eb
Hack Reactor Students <walkermellema@gmail.com>
2014-06-02 12:15:09 -0700

    almost done, gotta check first

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,44 +6,31 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
-  
-  var arr = [];
-  
-  var $body = $(document.body);
-  
-  $($body).each(function(index, element){
-    if(element.classList.contains(className)){
-      arr.push(element);
-    }
-    if(element.hasChildNodes()){
-      var $element = $(element.childNodes);
-      $($element).each(function(ind, elem){
-      
-        if(elem.classList && elem.classList.contains(className)){
-          arr.push(elem);
+
+  var nodes = [];
+
+  var checkNodes = function(nod){
+
+    if(nod.classList){
+      for(var i = 0; i < nod.classList.length; i++){
+
+        if(nod.classList[i] === className) {
+          nodes.push(nod);
         }
-        if(elem.hasChildNodes()){
-          getElemsByCName(className, arr, elem);
-        }  
-      });
-    }
-  });
-  
-  function getElemsByCName(cName, array1, el){
-    if(el.classList.contains(cName)){
-      array1.push(el);
-    }
-    if(el.hasChildNodes()){
-      var $el = $(el.childNodes);
-      $($el).each(function(i, e){
-        if(e.hasChildNodes()){
-          getElemsByCName(cName, array1, e);
+
+        if (nod.childNodes.length) {
+          for (var j = 0; j < nod.childNodes.length; j += 1) {
+            checkNodes(nod.childNodes[j]);
+          }
         }
-      });
+      }
     }
-  }
-  
-  
-  
-  return arr;
+  };
+
+  checkNodes(document.body);
+
+
+
+
+  return nodes;
 };
